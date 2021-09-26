7e72dbb5b33da759f2d680c9ea841ff5d055f72d
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-09-26 17:13:38 -0300

    Adding Assets

--- INF221/PSP1/src/com/lucasjoviniano/main/Avaliador.java
+++ INF221/PSP1/src/com/lucasjoviniano/main/Avaliador.java
@@ -11,17 +11,17 @@ public class Avaliador {
     List<Integer> atacantes;
     List<Integer> defensores;
 
-    public Avaliador(Integer A, Integer B, List<Integer> vA, List<Integer> vB) {
-        numAtacantes = A;
-        numDefensores = B;
-        atacantes = new Vector<>(vA);
-        defensores = new Vector<>(vB);
-    }
-
-    public char avalia() {
+    public Avaliador(Integer numAtacantes, Integer numDefensores, List<Integer> atacantes, List<Integer> defensores) {
+        this.numAtacantes = numAtacantes;
+        this.numDefensores = numDefensores;
+        this.atacantes = new Vector<>(atacantes);
+        this.defensores = new Vector<>(defensores);
 
         Collections.sort(atacantes);
         Collections.sort(defensores);
+    }
+
+    public char avalia() {
 
         if (atacantes.get(0) < defensores.get(1)) {
             return 'Y';
