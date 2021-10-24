7b37b7789875a8532d0e94d4957324a4d7edbf24
Lucas Joviniano <lucasjoviniano1@gmail.com>
2021-10-24 17:29:19 -0300

    Adicionado projeto prático

--- INF230/Trabalho2/rsa.py
+++ INF230/Trabalho2/rsa.py
@@ -1,5 +1,6 @@
 import itertools
 import random
+import sys
 from typing import Iterable, Tuple, TypeVar
 
 T = TypeVar("T")
@@ -8,27 +9,55 @@ MAX_INT = (2 ** 31) - 1
 
 
 def main():
-    p, q = generate_keys()
 
-    n: int = p * q
+    if sys.argv[1] == 'gerar':
+        p, q = generate_keys()
 
-    phi: int = (p - 1) * (q - 1)
+        n: int = p * q
 
-    e: int = get_e(phi)
+        phi: int = (p - 1) * (q - 1)
 
-    d: int = get_d(e, phi)
+        e: int = get_e(phi)
+
+        d: int = get_d(e, phi)
+
+        print("E: {}, D: {}, N: {}".format(e, d, n))
+
+    elif sys.argv[1] == 'codificar':
+
+        mensagem, n, e = sys.argv[2], int(sys.argv[3]), int(sys.argv[4])
+
+        coded = encode(mensagem, n, e)
+
+        print(str(coded)[1:-1])
+
+    elif sys.argv[1] == 'decodificar':
+        mensagem, n, d = "1634805184089914118, 437016238775307267, 2458304341065466193", 2752537950741231113, 492940772177008613
+
+        decoded = decode([x.strip() for x in mensagem.split(',')], int(d), int(n))
+
+        print(decoded)
+
 
     message: str = 'TAYLORSWIFTRAINHADOPOP'
-
-    print("P: {}, Q: {}, E: {}, D: {}".format(p, q, e, d))
-
-    coded = encode(message, n, e)
-
-    print(coded)
-
-    decoded = decode(coded, d, n)
-
-    print(decoded)
+    #
+    #
+    #
+    # coded = encode(message, n, e)
+    #
+    # print("Mensagem Codificada: {}".format(coded))
+    #
+    # decoded = decode(coded, d, n)
+    #
+    # print("Mensagem Decodificada: {}".format(decoded))
+    #
+    # signed = sign(message, d, n)
+    #
+    # print("Mensagem Assinada: {}".format(signed))
+    #
+    # unsigned = check_sign(signed, e, n)
+    #
+    # print("Mensagem Conferida: {}".format(unsigned))
 
 
 def get_e(phi: int) -> int:
@@ -48,13 +77,40 @@ def get_d(e: int, phi: int) -> int:
     return s
 
 
+def sign(message, d, n):
+    block_size = get_block_size(n)
+
+    blocks = generate_message_blocks(message, block_size)
+
+    return [mod_pow(int(x), d, n) for x in blocks]
+
+
+def check_sign(message, e, n):
+    codes = [str(mod_pow(int(x), e, n)) for x in message]
+    message = ''
+
+    for i, k in enumerate(codes):
+        if len(k) % 2 != 0:
+            codes[i] = '0' + k
+
+    for k in codes:
+        for a, b in grouped(k):
+            message += get_char_from_code(a + b)
+
+    return message
+
+
 def encode(message: str, key: int, e: int) -> [str]:
     block_size = get_block_size(key)
 
-    blocks = []
-    k = 0
+    blocks = generate_message_blocks(message, block_size)
 
-    print("Generating Blocks")
+    return [mod_pow(int(x), e, key) for x in blocks]
+
+
+def generate_message_blocks(message, block_size):
+    k = 0
+    blocks = []
 
     for _ in message:
         block = []
@@ -65,20 +121,18 @@ def encode(message: str, key: int, e: int) -> [str]:
         blocks.append(''.join(block))
 
     blocks = list(filter(None, blocks))
-    print(blocks)
 
-    return [mod_pow(int(x), e, key) for x in blocks]
+    return blocks
 
 
 def decode(blocks: [int], d: int, n: int) -> str:
+
     codes = [str(mod_pow(int(x), d, n)) for x in blocks]
     message = ''
 
     for i, k in enumerate(codes):
         if len(k) % 2 != 0:
             codes[i] = '0' + k
-
-    print(codes)
 
     for k in codes:
         for a, b in grouped(k):
@@ -107,7 +161,6 @@ def get_char_code(char: str) -> str:
 
 
 def get_block_size(n: int) -> int:
-    print("Getting block size")
     i = "25" * len(str(n))
 
     while int(i) > n:
