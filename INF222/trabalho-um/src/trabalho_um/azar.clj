f96cfafd4ca55f5f8aa094388ce2fc10361e875a
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-04 18:17:03 -0300

    [INF222] Corrige erro de quem ganha

--- INF222/trabalho-um/src/trabalho_um/azar.clj
+++ INF222/trabalho-um/src/trabalho_um/azar.clj
@@ -1,13 +1,19 @@
 (ns trabalho-um.azar (:gen-class))
 
+; Quantas vezes altera o jogador que está na frente
+; Ficou na frente o jogo todo?
+; Em quantos lançamentos ele ficou na frente
+
 (defn toss-coin "Joga moeda" [] (rand-nth '(:cara :coroa)))
 
 (defn azar
   [first-amount second-amount]
   (loop [v1 first-amount v2 second-amount rounds 1]
     (let [coin (toss-coin)]
-      (if (or (zero? v1) (zero? v2))
-        rounds
+      (if (zero? v1) 
+        [:segundo rounds]
+        (if (zero? v2)
+          [:primeiro rounds]
         (if (= coin :cara)
-          (recur (dec v1) v2 (inc rounds))
-          (recur v1 (dec v2) (inc rounds)))))))
+          (recur (dec v1) (inc v2) (inc rounds))
+          (recur (inc v1) (dec v2) (inc rounds))))))))
