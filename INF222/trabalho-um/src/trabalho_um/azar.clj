934ce551a03ccf2c6d972587f8512a4079497cf3
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-05 10:02:52 -0300

    [INF222] Refatoração de Keywords para strings

--- INF222/trabalho-um/src/trabalho_um/azar.clj
+++ INF222/trabalho-um/src/trabalho_um/azar.clj
@@ -8,11 +8,11 @@
 
 (defn azar
   [first-amount second-amount]
-  (loop [ans {:v1 first-amount :v2 second-amount :winner :primeiro :rounds 1 :one-swaps 0 :two-swaps 0}]
+  (loop [ans {:v1 first-amount :v2 second-amount :winner "" :rounds 1 :one-swaps 0 :two-swaps 0}]
     (let [coin (toss-coin)]
       (cond
-        (zero? (get ans :v1)) (assoc ans :winner :segundo)
-        (zero? (get ans :v2)) (assoc ans :winner :primeiro)
+        (zero? (get ans :v1)) (assoc ans :winner "segundo")
+        (zero? (get ans :v2)) (assoc ans :winner "primeiro")
         :else (case coin
                 ::head (recur (-> ans
                                   (update :two-swaps #(if (= (get ans :v1) (get ans :v2)) (inc %) %))
@@ -20,6 +20,7 @@
                                   (update :v2 inc)
                                   (update :rounds inc)))
                 ::tail (recur (-> ans
+                                  ; Quem está na frente só altera se os valores forem iguais
                                   (update :one-swaps #(if (= (get ans :v1) (get ans :v2)) (inc %) %))
                                   (update :v1 inc)
                                   (update :v2 dec)
