5ac96008cdc892365157d1752a269df91226c89c
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-05 14:44:01 -0300

    [INF222] Reformata a limpeza da saída e adiciona documentação

--- INF222/trabalho-um/src/trabalho_um/azar.clj
+++ INF222/trabalho-um/src/trabalho_um/azar.clj
@@ -2,7 +2,18 @@
 
 (defn toss-coin "Joga moeda" [] (rand-nth '(::head ::tail)))
 
-(defn check-swap [v ans] (if (= (ans :v1) (ans :v2)) (inc v) v))
+(defn check-swap
+  "Decide se quem estava na frente mudou. Só acontece quando ambos forem iguais"
+  [v ans]
+  (if (= (ans :v1) (ans :v2)) (inc v) v))
+
+(defn clear-answer
+  "Limpa os valores desnecessários de fim de jogo"
+  [ans winner]
+  (-> ans
+      (dissoc :v1)
+      (dissoc :v2)
+      (assoc :winner winner)))
 
 (defn azar
   [first-amount second-amount]
@@ -14,8 +25,8 @@
               :two-swaps 0}]
     (let [coin (toss-coin)]
       (cond
-        (zero? (ans :v1)) (-> ans (dissoc :v1) (dissoc :v2) (assoc :winner "segundo"))
-        (zero? (ans :v2)) (-> ans (dissoc :v1) (dissoc :v2) (assoc :winner "primeiro"))
+        (zero? (ans :v1)) (clear-answer ans "segundo")
+        (zero? (ans :v2)) (clear-answer ans "primeiro")
         :else (case coin
                 ::head (recur (-> ans
                                   (update :two-swaps check-swap ans)
