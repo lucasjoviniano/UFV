998ca2a13a08cf16c2996fbd3c1879827327fa1b
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-05 22:39:34 -0300

    [INF222] Redireciona saídas para arquivos csv

--- INF222/trabalho-um/src/trabalho_um/core.clj
+++ INF222/trabalho-um/src/trabalho_um/core.clj
@@ -1,6 +1,9 @@
 (ns trabalho-um.core
-  (:gen-class)
-  (:use [trabalho-um.azar]))
+  (:gen-class))
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io]
+         '[trabalho-um.azar :as azar]
+         '[clojure.set :as set])
 
 (defn get-probabilities
   "Gera uma quantidade definina por amount de tentativas"
@@ -21,7 +24,19 @@
   (frequencies (repeatedly amount (fn [] (duel-battle amount player-one player-two)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Decide a atividade e escreve os resultados em arquivos .csv"
   [& args]
-  (doseq [arg args]
-    (println (duel (Integer/parseInt arg) (/ 4 6) (/ 5  6)))))
+  (case (first args)
+    "duelo" (let [[amount f-n f-d s-n s-d] (map #(Integer/parseInt %) (rest args))]
+              (with-open [writer (io/writer "duelo.csv")]
+                (csv/write-csv writer (set/rename-keys
+                                       (duel amount (/ f-n f-d) (/ s-n s-d))
+                                       {:player-one "player-one" :player-two "player-two"}))))
+
+    "azar" (let [[amount v-one v-two] (map #(Integer/parseInt %) (rest args))
+                 runs (repeatedly amount (fn [] (azar/azar v-one v-two)))
+                 columns [:winner :rounds :one-swaps :two-swaps]
+                 headers (map name columns)
+                 rows (mapv #(mapv % columns) runs)]
+             (with-open [writer (io/writer "azar.csv")]
+               (csv/write-csv writer (cons headers rows))))))
