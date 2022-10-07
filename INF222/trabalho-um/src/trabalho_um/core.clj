1a3f740aaca7e38f307b1f29c9ed7a483f5f52a4
Lucas Joviniano <lucasjoviniano1@gmail.com>
2022-10-07 11:11:10 -0300

    [INF222] Estratégia 2

--- INF222/trabalho-um/src/trabalho_um/core.clj
+++ INF222/trabalho-um/src/trabalho_um/core.clj
@@ -15,8 +15,8 @@
 (defn battle
   [probability player-one player-two]
   (cond
-    (<= (first probability) (:aim player-one)) :player-one
-    (<= (second probability) (:aim player-two)) :player-two
+    (< (first probability) (:aim player-one)) :player-one
+    (< (second probability) (:aim player-two)) :player-two
     :else :none))
 
 (defn duel-battle
@@ -47,9 +47,37 @@
           :none (recur p-three p-two p-one (get-probabilities 2) false))
         ))))
 
+(defn strategy-two
+  [player-one player-two player-three]
+  (loop [p-one player-one
+         p-two player-two
+         p-three player-three
+         probability (get-probabilities 2)
+         one-dead false]
+    (let [winner (battle probability p-one p-two)]
+      (if one-dead
+        (case winner
+          :player-one (:name p-one)
+          :player-two (:name p-two)
+          :none (recur p-one p-two p-three (get-probabilities 2) true))
+        (case winner
+          :player-one (recur p-three p-one p-two (get-probabilities 2) true)
+          :player-two (recur p-three p-two p-one (get-probabilities 2) true)
+          :none (recur p-one p-two p-three (get-probabilities 2) false))))))
+
 (defn duel [amount player-one player-two]
   (frequencies (repeatedly amount (fn [] (duel-battle (Player. "A" player-one)
                                                       (Player. "B" player-two))))))
+
+(defn truel [strategy amount player-one player-two player-three]
+  (case strategy
+    1 (frequencies (repeatedly amount (fn [] (strategy-one (Player. "A" player-one)
+                                                           (Player. "B" player-two)
+                                                           (Player. "C" player-three)))))
+
+    2 (frequencies (repeatedly amount (fn [] (strategy-two (Player. "A" player-one)
+                                                           (Player. "B" player-two)
+                                                           (Player. "C" player-three)))))))
 
 (defn -main
   "Decide a atividade e escreve os resultados em arquivos \".csv\""
@@ -60,6 +88,11 @@
                 (csv/write-csv writer (set/rename-keys
                                         (duel amount (/ f-n f-d) (/ s-n s-d))
                                         {:player-one "player-one" :player-two "player-two"}))))
+
+    "truelo" (let [[amount s f-n f-d s-n s-d t-n t-d] (map #(Integer/parseInt %) (rest args))]
+               (with-open [writer (io/writer "truelo.csv")]
+                 (csv/write-csv writer (truel s amount (/ f-n f-d) (/ s-n s-d) (/ t-n t-d)))))
+
 
     "azar" (let [[amount v-one v-two] (map #(Integer/parseInt %) (rest args))
                  runs (repeatedly amount (fn [] (azar/azar v-one v-two)))
